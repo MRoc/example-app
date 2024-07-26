5012d5adfa32eb3f48ad9165528c93acf9596797
Matthias Mayrock <mail@mroc.de>
2024-07-26 14:49:47 +0200

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # example-app
-Simple example app that can be used for kubernetes tests
+
+Simple example app that can be used for kubernetes tests including metrics. To start follow:
+
+```bash
+npm install
+npm run start
+```
+
+Then visit the following endpoints:
+
+* http://localhost:3000/
+* http://localhost:3000/comments
+* http://localhost:3000/threads
+* http://localhost:3000/replies
+* http://localhost:3000/swagger-stats/metrics
---
+++ index.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const swStats = require('swagger-stats');
+
+const app = express();
+app.use(swStats.getMiddleware());
+
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
+
+app.get("/comments", (req, res) => {
+  res.send("Comments");
+});
+
+app.get("/threads", (req, res) => {
+  res.send("Threads");
+});
+
+app.get("/replies", (req, res) => {
+  res.send("replies");
+});
+
+const port = 3000;
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
